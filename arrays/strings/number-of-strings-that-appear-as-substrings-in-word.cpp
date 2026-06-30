b45496c9c30731c2a4077694fa1a7a7a41ba0ba1
Vivek Teja <vivekteja0308@gmail.com>
2026-06-30 11:07:37 +0530

    Implement function to count substring patterns in word

---
+++ arrays/strings/number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numOfStrings(vector<string>& patterns, string word) {
+        int count=0;
+        for(const string& p: patterns)
+        {
+            if(word.find(p)!=string::npos)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
