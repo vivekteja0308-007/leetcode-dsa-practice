ae574571549c2a114162479c238935a77cc78a68
Vivek Teja <vivekteja0308@gmail.com>
2026-09-20 23:26:13 +0530

    Create assign cookies.cpp

---
+++ assign cookies.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>&  g, vector<int>& s)
+    {
+sort(g.begin(), g.end());
+sort(s.begin(), s.end());
+int i = 0; // child    
+int j = 0; // cookie    
+while (i < g.size() && j < s.size())    
+{    
+    if (g[i]<=s[j])    
+    {    
+        i++;    
+        j++;    
+    }    
+    else    
+    {    
+        j++;    
+    }    
+}    
+return i;
+    }
+};
